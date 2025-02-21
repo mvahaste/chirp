dcf4c77037061db50477759f6569342ad448c773
mvahaste <mvahaste@proton.me>
2025-02-21 20:23:37 +0200

    Homepage cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -14,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Chirp",
+  description: "The best bird-themed social media platform.",
 };
 
 const geistSans = Geist({
@@ -37,33 +36,30 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+          <main className="flex min-h-screen flex-col items-center">
+            <div className="flex w-full flex-1 flex-col items-center gap-20">
+              <header className="border-grid sticky top-0 z-50 flex h-16 w-full justify-center border-b border-b-foreground/10 bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
+                  <div className="flex items-center gap-5 font-semibold">
+                    <Link href={"/"}>Chirp</Link>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
+              </header>
+              <div className="flex max-w-5xl flex-grow flex-col gap-20 p-5">
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="mx-auto flex w-full items-center justify-center gap-6 pb-4 pt-8 text-center text-xs">
                 <p>
-                  Powered by{" "}
+                  View the source code on{" "}
                   <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    href="https://github.com/mvahaste/chirp"
                     target="_blank"
                     className="font-bold hover:underline"
                     rel="noreferrer"
                   >
-                    Supabase
+                    GitHub
                   </a>
                 </p>
                 <ThemeSwitcher />
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,61 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Home() {
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
+    <main className="flex flex-1 flex-col gap-6 px-4">
+      <p>
+        Nisi magna occaecat est in. Anim officia enim ullamco magna id quis esse
+        aute sint eiusmod eiusmod mollit id fugiat Lorem. Ullamco occaecat irure
+        adipisicing in. Ea nostrud do excepteur ipsum sunt. Consequat dolor
+        voluptate consectetur incididunt elit esse labore dolore proident.
+        Veniam incididunt ad amet consectetur minim ipsum elit occaecat
+        consequat elit. Irure excepteur elit commodo. Consequat Lorem labore
+        laborum culpa aliqua ea cillum quis mollit ex. Commodo aute nulla enim
+        ea non voluptate enim. Lorem enim ut incididunt cillum ullamco velit.
+        Voluptate aliquip ex magna eu pariatur aute sint magna non proident
+        proident laborum commodo. Elit aliqua ea proident nulla nulla aliquip id
+        ad duis ea id duis sint nisi. Dolore occaecat esse elit ipsum voluptate
+        est aute in ea laborum Lorem veniam labore proident. Occaecat laboris
+        qui enim deserunt proident voluptate commodo sit aute ut. Incididunt
+        dolore labore pariatur in voluptate ut. Laboris ex aliquip cupidatat sit
+        aliquip officia do est proident enim et ullamco ipsum. Aliqua cupidatat
+        pariatur sit voluptate nostrud exercitation adipisicing pariatur ea
+        commodo cillum culpa sint irure. Aute non Lorem veniam deserunt fugiat
+        et elit cupidatat. Ea qui pariatur ullamco et. Commodo ex irure
+        reprehenderit amet exercitation consectetur laboris eu aute
+        reprehenderit consectetur occaecat non. Velit consectetur ut do aute.
+        Culpa excepteur ut qui culpa ullamco proident duis pariatur adipisicing
+        sint ea. Nisi Lorem commodo reprehenderit culpa. Elit duis Lorem nisi
+        excepteur nostrud nulla eiusmod eiusmod aute sunt ex voluptate dolore
+        id. Deserunt non consequat sit in dolore pariatur nostrud laboris
+        voluptate Lorem aliquip reprehenderit aliquip ut. Dolore officia
+        exercitation commodo magna ad ullamco exercitation in non incididunt
+        exercitation ullamco reprehenderit voluptate. Aute officia veniam dolore
+        anim nostrud duis laborum velit. Reprehenderit elit duis reprehenderit.
+        Cillum officia ipsum consectetur labore excepteur occaecat. Exercitation
+        excepteur sunt consectetur culpa tempor cupidatat sit. Reprehenderit
+        enim sunt velit. Consequat labore reprehenderit nostrud veniam
+        incididunt sint labore non esse. Nisi aliquip ut do velit deserunt et
+        aute reprehenderit dolore pariatur. Adipisicing aute ipsum non sunt ad
+        magna minim mollit eiusmod ut ut. Eiusmod consequat eiusmod fugiat
+        tempor nostrud cillum eu velit consectetur. Do ipsum irure cupidatat
+        nisi aliquip culpa aliqua sit ipsum. Eiusmod labore magna cupidatat
+        occaecat anim enim nostrud est anim consectetur ut et nostrud laborum
+        deserunt. Sint nulla magna officia aute reprehenderit mollit excepteur
+        proident nulla voluptate magna cillum deserunt fugiat. Dolore ipsum
+        deserunt eiusmod magna anim cillum labore culpa deserunt quis minim.
+        Consequat magna proident laborum cupidatat velit fugiat laborum. Magna
+        reprehenderit duis exercitation Lorem ullamco veniam labore ipsum velit
+        qui ullamco minim ea ipsum. Culpa est qui ut magna adipisicing elit
+        incididunt deserunt duis est. Non exercitation sit fugiat id aute duis
+        quis quis irure. Dolore consequat excepteur ut laborum magna occaecat
+        laborum Lorem minim laborum duis elit eu non nisi. Tempor fugiat nostrud
+        cillum labore occaecat qui ea nostrud nostrud cillum aliquip sit nulla
+        nisi. Sint culpa qui ullamco sint Lorem do non ad et. Laborum do minim
+        laboris. Cupidatat est Lorem tempor ut magna laborum aliquip ut fugiat
+        veniam cillum fugiat deserunt laboris. Minim id cillum ut. Magna ut
+        nulla do. Do minim cupidatat magna dolor cupidatat. Consequat eiusmod
+        mollit proident incididunt officia veniam commodo irure incididunt minim
+        culpa fugiat quis.
+      </p>
+    </main>
   );
 }
