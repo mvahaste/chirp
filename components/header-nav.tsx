9b9d2f0276db394c0cd4de39ae2d92ff74ac910d
mvahaste <mvahaste@proton.me>
2025-02-21 23:48:44 +0200

    Profile nav link depends on username

--- components/header-nav.tsx
+++ components/header-nav.tsx
@@ -1,14 +1,35 @@
 "use client";
 
+import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function HeaderNav() {
+  const [username, setUsername] = useState<string | null>(null);
+
   const pathname = usePathname();
+
+  useEffect(() => {
+    const getUsername = async () => {
+      const supabase = createClient();
+      const user = await supabase.auth.getUser();
+      const { data } = await supabase
+        .from("profiles")
+        .select("username")
+        .eq("id", user.data.user?.id);
+
+      if (data && data.length > 0) {
+        setUsername(data[0].username);
+      }
+    };
+
+    getUsername();
+  }, []);
 
   const links = {
     "/": "Home",
-    "/profile": "Profile",
+    "<USERNAME>": "Profile",
   };
 
   return (
@@ -16,7 +37,7 @@ export default function HeaderNav() {
       {Object.entries(links).map(([path, label]) => (
         <Link
           key={path}
-          href={path}
+          href={path == "<USERNAME>" ? `/${username}` : path}
           className={pathname == path ? "font-semibold" : ""}
         >
           {label}
