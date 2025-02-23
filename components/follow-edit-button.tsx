1e0e62243ba28a26669d02f99e0ea0ffbf51584f
mvahaste <mvahaste@proton.me>
2025-02-23 23:35:18 +0200

    Edit profile UI

--- components/follow-edit-button.tsx
+++ components/follow-edit-button.tsx
@@ -4,6 +4,17 @@ import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/client";
 import { followUserAction, unfollowUserAction } from "@/app/actions";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { SubmitButton } from "./submit-button";
 
 interface FollowEditButtonProps {
   user_id: string;
@@ -55,18 +66,55 @@ export default function FollowEditButton({
   };
 
   return (
-    <Button
-      className="absolute right-0 rounded-full"
-      variant="outline"
-      onClick={() => {
-        if (is_self) {
-          // Edit profile
-        } else {
-          handleFollow();
-        }
-      }}
-    >
-      {followEditText()}
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className="absolute right-0 rounded-full"
+          variant="outline"
+          onClick={() => {
+            if (is_self) {
+              // Edit profile
+            } else {
+              handleFollow();
+            }
+          }}
+        >
+          {followEditText()}
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Profile</DialogTitle>
+          <DialogDescription className="sr-only">
+            Edit your display name, bio, avatar, and cover image.
+          </DialogDescription>
+        </DialogHeader>
+        <form className="flex flex-col gap-1.5 [&>input]:mb-3">
+          <Label htmlFor="name">Name</Label>
+          <Input name="name" type="text" placeholder="Your name" />
+          <Label htmlFor="bio">Bio</Label>
+          <Input name="bio" type="text" placeholder="A little about yourself" />
+          <Label htmlFor="avatar">Avatar</Label>
+          <Input
+            name="avatar"
+            type="file"
+            placeholder="https://example.com/avatar.png"
+          />
+          <Label htmlFor="cover">Cover</Label>
+          <Input
+            name="cover"
+            type="file"
+            placeholder="https://example.com/cover.png"
+          />
+          <SubmitButton
+            formAction=""
+            pendingText="Saving..."
+            className="rounded-full"
+          >
+            Save
+          </SubmitButton>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
